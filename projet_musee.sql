316b9f17196b89272afb97a187ccc130658a0c16
nocxy73 <erwann.melech33@gmail.com>
2025-03-13 10:58:32 +0100

    Update projet_musee.sql

--- projet_musee.sql
+++ projet_musee.sql
@@ -1,8 +1,9 @@
 
 use projet_musee;
 create table exposition (
-num_exposition int  unsigned primary key not null auto_increment,
-id_exposition int
+id_exposition int primary key not null,
+descriptif varchar(1000),
+libelle varchar(50)
 );
 
 create table visiteur (
@@ -11,21 +12,19 @@ nom varchar(15),
 prenom varchar(15),
 age int,
 tel int,
-mail varchar(50)
+mail varchar(50),
+h_arrivee datetime,
+h_depart datetime
 );
 
-create table tickets (
-num_ticket int unsigned primary key not null auto_increment,
-id_ticket int  unsigned not null
+create table type_tickets (
+id_ticket int  unsigned not null  primary key,
+libelle varchar(50)
 );
 
 create table visite (
-id_visite int  unsigned primary key not null auto_increment,
 id_visiteur int,
 id_exposition int,
-id_ticket int,
-h_arrivee datetime,
-h_depart datetime,
 constraint fk_visiteur foreign key (id_visiteur) references visiteur (id_visiteur),
 constraint fk_exposition foreign key (id_exposition) references exposition (id_exposition),
 constraint fk_ticket foreign key (id_ticket) references tickets (id_ticket)
